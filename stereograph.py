4364f609d35fcf1154089ff7bc60cd32bc31cb89
Böhnke, Christian <christian@home-boehnke.de>
2020-05-30 12:25:50 +0200

    Provide an icon

--- stereograph.py
+++ stereograph.py
@@ -169,7 +169,7 @@ class Stereograph:
     def initGui(self):
         """Create the menu entries and toolbar icons inside the QGIS GUI."""
 
-        icon_path = ':/plugins/stereograph/icon.png'
+        icon_path = ':/plugins/stereograph/icons/main.png'
         self.add_action(
             icon_path,
             text=self.tr(u'Stereograph'),
